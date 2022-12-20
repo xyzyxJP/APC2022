9e8275889f65520895eb7bb25ee8b1ed7da51c33
xyzyxJP <sahashi.yuto.21@shizuoka.ac.jp>
2022-12-20 15:07:48 +0900

    Add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       title: 'APC2022',
       theme: ThemeData(
         useMaterial3: true,
+        fontFamily: 'Murecho',
       ),
       home:
           // const HomePage(title: 'APC2022 Home Page'),
