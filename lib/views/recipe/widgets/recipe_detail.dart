b309db770fb5925aca7d847ea8b21237741e4684
sl21ku <sl2111ku@gmail.com>
2022-12-22 17:50:01 +0900

    レシピ詳細画面-更新+googleffont導入
    カロリー、お金、時間の表示
    写真の位置変更
    google_font導入
    作り方

--- lib/views/recipe/widgets/recipe_detail.dart
+++ lib/views/recipe/widgets/recipe_detail.dart
@@ -1,6 +1,7 @@
 import 'package:apc2022/models/recipe_detail.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:video_player/video_player.dart';
 
@@ -31,7 +32,7 @@ class _RecipeDetailModalState extends State<RecipeDetailModal> {
   Widget build(BuildContext context) {
     final recipe = widget.recipe;
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(18.0),
       child: Expanded(
         child: ListView(
           controller: widget.scrollController,
@@ -42,10 +43,35 @@ class _RecipeDetailModalState extends State<RecipeDetailModal> {
                 alignment: Alignment.center,
                 child: Text(
                   recipe.title!,
-                  style: const TextStyle(fontSize: 24.0),
+                  style: GoogleFonts.yuseiMagic(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
                 ),
               ),
             ),
+            Align(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color.fromARGB(255, 90, 90, 90)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: CachedNetworkImage(
+                      imageUrl: recipe.squareVideo!.posterUrl!,
+                      errorWidget: (context, url, dynamic error) =>
+                          const Icon(Icons.error),
+                    ),
+                  ),
+                ),
+                _buildCalYenTime(recipe),
+              ],
+            )),
             Expanded(
               child: ListView.builder(
                 shrinkWrap: true,
@@ -54,14 +80,6 @@ class _RecipeDetailModalState extends State<RecipeDetailModal> {
                 itemBuilder: (context, index) {
                   return _buildIngredientGroup(recipe.ingredientGroups![index]);
                 },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: CachedNetworkImage(
-                imageUrl: recipe.squareVideo!.posterUrl!,
-                errorWidget: (context, url, dynamic error) =>
-                    const Icon(Icons.error),
               ),
             ),
             Center(
@@ -84,6 +102,18 @@ class _RecipeDetailModalState extends State<RecipeDetailModal> {
                 _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
               ),
             ),
+            //作り方
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: recipe.recipeSteps!.length,
+                itemBuilder: (context, index) {
+                  return _buildRecipeSteps(recipe.recipeSteps![index]);
+                },
+              ),
+            ),
+
             ElevatedButton(
               onPressed: () {
                 launchUrl(
@@ -111,14 +141,97 @@ class _RecipeDetailModalState extends State<RecipeDetailModal> {
     );
   }
 
-  Widget _buildIngredientGroup(IngredientGroups? ingredientGroups) {
-    return Padding(
+  Container _buildCalYenTime(Data recipe) {
+    return Container(
+      height: 200,
+      width: 120,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Color.fromARGB(43, 5, 54, 83),
+        ),
+        color: Color.fromARGB(15, 143, 201, 240),
+      ),
       padding: const EdgeInsets.all(8.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            ingredientGroups!.name ?? '名前なし',
-            style: const TextStyle(fontSize: 16.0),
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Icon(Icons.whatshot),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              recipe.nutrient!.items!.first.amount!,
+              style: GoogleFonts.yuseiMagic(
+                // fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(5.0)),
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Icon(Icons.currency_yen),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              recipe.cookingCost!.substring(5),
+              style: GoogleFonts.yuseiMagic(
+                // fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(5.0)),
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Icon(Icons.timer),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              recipe.cookingTime!.substring(5),
+              style: GoogleFonts.yuseiMagic(
+                // fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIngredientGroup(IngredientGroups? ingredientGroups) {
+    return Padding(
+      padding: const EdgeInsets.all(36.0),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              height: 30,
+              width: 90,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(
+                  color: Color.fromARGB(59, 0, 0, 0),
+                ),
+                color: Color.fromARGB(15, 143, 201, 240),
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  ingredientGroups!.name ?? '材料',
+                  style: GoogleFonts.yuseiMagic(
+                    fontSize: 19,
+                  ),
+                ),
+              ),
+            ),
           ),
           ListView.builder(
             shrinkWrap: true,
@@ -130,12 +243,68 @@ class _RecipeDetailModalState extends State<RecipeDetailModal> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(ingredientGroups.recipeIngredients![index].name!),
-                    Text(ingredientGroups.recipeIngredients![index].servings1!)
+                    Text(ingredientGroups.recipeIngredients![index].name!,
+                        style: GoogleFonts.yuseiMagic(
+                          fontSize: 18,
+                        )),
+                    Text(ingredientGroups.recipeIngredients![index].servings1!,
+                        style: GoogleFonts.yuseiMagic(
+                          fontSize: 18,
+                        )),
                   ],
                 ),
               );
             },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRecipeSteps(RecipeSteps? recipeSteps) {
+    return Padding(
+      padding: const EdgeInsets.all(36.0),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        border:
+                            Border.all(color: Color.fromARGB(90, 11, 0, 172)),
+                        color: Color.fromARGB(19, 143, 201, 240),
+                      ),
+                      child: Column(
+                        children: [
+                          const Text(
+                            'step',
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            recipeSteps!.step!.toString(),
+                            style: GoogleFonts.yuseiMagic(
+                              fontSize: 19,
+                            ),
+                          ),
+                        ],
+                      )),
+                  Text(
+                    recipeSteps!.description!,
+                    style: GoogleFonts.yuseiMagic(
+                      fontSize: 19,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
