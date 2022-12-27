3243e546bfe5f02a77281098f69131734b20dcef
yui963 <95596287+yui963@users.noreply.github.com>
2022-12-27 22:30:13 +0900

    起動画面初稿

--- lib/views/settings/settings.dart
+++ lib/views/settings/settings.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:apc2022/models/recipe.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  final String categoryId;
+  final String categoryName;
+  final List<Recipe> recipeList;
+
+  const SettingsPage({
+    super.key,
+    required this.categoryId,
+    required this.categoryName,
+    required this.recipeList,
+  });
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -11,6 +23,10 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.categoryName),
+      ),
+    );
   }
 }
